610ab5fd650994265afc8df0db8a3ac4a155a860
Lucas Bonfim <aprendiz113@gazin.net.br>
2022-12-22 11:03:32 -0300

    estilo no cadastro

--- src/views/partials/header.php
+++ src/views/partials/header.php
@@ -1,9 +1,7 @@
-
 <html lang="pt-br">
 
 <head>
   <meta charset="UTF-8">
-
   <link href="<?= $base ?>/css/bootstrap.min.css" rel="stylesheet">
   <link href="<?= $base ?>/css/select2.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="<?= $base; ?>/css/style.css">
@@ -12,15 +10,14 @@
 
 <body>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Gazin</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3 fs-6" href="#">Gazin - Clientes</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="navbar-nav">
       <a class="nav-link px-3 text-white" href="<?= $base; ?>/sair">Sair</a>
     </div>
-  </div>
-</header>
-<script>
-</script>
+    </div>
+  </header>
+  <script>
+  </script>
